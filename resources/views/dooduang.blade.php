815d15cd3f00c9acc9a7d2efce49f01149cc1921
keittisak <keittisak@movefast.me>
2019-05-21 00:13:13 +0700

    update

--- resources/views/dooduang.blade.php
+++ resources/views/dooduang.blade.php
@@ -122,7 +122,7 @@
     </div>
 
     <div class="modal fade" id="loading-modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered modal-xl" role="document">
+        <div class="modal-dialog modal-dialog-centered modal-sm" role="document">
             <div class="modal-content">
             <div class="modal-body">
                 <center>
