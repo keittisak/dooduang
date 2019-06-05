7f4ca676c7d3b2449362aa0455205663782ef383
keittisak <keittisak@movefast.me>
2019-06-05 14:54:18 +0700

    update

--- resources/views/dooduang.blade.php
+++ resources/views/dooduang.blade.php
@@ -3,7 +3,7 @@
 <head>
   <title>WITCH CARD</title>
   <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   {{--  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">  --}}
   <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
   <link href="https://fonts.googleapis.com/css?family=Athiti&subset=thai,latin" rel='stylesheet' type='text/css'>
